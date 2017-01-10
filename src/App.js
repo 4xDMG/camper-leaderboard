c6558c54fc9d229c399c1e17dde4b02e5777a43c
4xDMG <dajtait87@gmail.com>
2017-01-10 17:23:07 +1000

    Tried to make the API call without using fetch()

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 import CamperList from './components/camper_list';
 
 const recentList = 'https://fcctop100.herokuapp.com/api/fccusers/top/recent';
-const alltimeList = 'https://fcctop100.herokuapp.com/api/fccusers/top/alltime';
+/*const alltimeList = 'https://fcctop100.herokuapp.com/api/fccusers/top/alltime';
+const fccInit = {
+  method: 'GET',
+  mode: 'cors',
+  cache: 'default'
+};*/
 
 class App extends Component {
   constructor(props) {
@@ -17,8 +22,39 @@ class App extends Component {
     this.getData(recentList);
   }
 
-  getData(url) {
-    fetch(url)
+  request = obj => {
+    return new Promise((resolve, reject) => {
+        let xhr = new XMLHttpRequest();
+        xhr.open(obj.method || "GET", obj.url);
+        if (obj.headers) {
+            Object.keys(obj.headers).forEach(key => {
+                xhr.setRequestHeader(key, obj.headers[key]);
+            });
+        }
+        xhr.onload = () => {
+            if (xhr.status >= 200 && xhr.status < 300) {
+                resolve(xhr.response);
+            } else {
+                reject(xhr.statusText);
+            }
+        };
+        xhr.onerror = () => reject(xhr.statusText);
+        xhr.send(obj.body);
+    });
+};
+
+  getData = (url) => {
+    this.request(url)
+      .then(data => {
+        console.log(data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  /*getData(url, fccInit) {
+    fetch(url, fccInit)
     .then(
       response => response.json()
     )
@@ -28,7 +64,7 @@ class App extends Component {
     .catch(
       error => console.log(error.message)
     );
-  }
+  }*/
   
   render() {
     return (
